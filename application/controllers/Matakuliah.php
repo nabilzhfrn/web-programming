2c4d79c82ddefd1a663d3c5ea621271d713c0e6f
nabilzhfrn <91109557+nabilzhfrn@users.noreply.github.com>
2021-11-19 16:38:00 +0700

    Tugas Pertemuan - 10

---
+++ application/controllers/Matakuliah.php
@@ -0,0 +1,55 @@
+<?php
+class Matakuliah extends CI_Controller
+{
+    function __construct()
+    {
+        parent::__construct();
+    }
+    public function index()
+    {
+        $this->load->view('view-form-matakuliah');
+    }
+    public function cetak()
+    {
+        $this->form_validation->set_rules(
+            'kode',
+            'Kode',
+            'required|min_length[5]|max_length[5]',
+            array(
+                'requried' => '%s harus diisi.',
+                'min_length' => '%s minimal 5 karakter',
+                'max_length' => '%s maksnimal 5 karakter'
+            )
+        );
+
+        $this->form_validation->set_rules(
+            'nama',
+            'Nama',
+            'required',
+            array(
+                'requried' => '%s harus diisi.',
+                'min_length' => '%s minimal 5 karakter'
+
+            )
+        );
+
+        $this->form_validation->set_rules(
+            'sks',
+            'SKS',
+            'required',
+            array('requried' => '%s harus diisi.')
+        );
+
+        if ($this->form_validation->run() == FALSE) {
+            $this->load->view('view-form-matakuliah');
+        } else {
+            $data = [
+                'kode' => $this->input->post('kode'),
+                'nama' => $this->input->post('nama'),
+                'sks' => $this->input->post('sks')
+            ];
+
+            $this->load->view('view-data-matakuliah', $data);
+        }
+    }
+}
---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Syafi</li>
+        <li>Nama Belakang : Kryan</li>
+        <li>Nama Lengkap : Disposable Face Mask 50pcs</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="square">
+        <li> Bekasi City of the Light, Olympus.st, No.666B</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="square">
+        <li> Bekasi City of the Light</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Berlari mencari kitab di barat</li>
+        <li>Berenang melintasi Segitiga Bermuda</li>
+    </ul>
+</section>
